d6c0f1a90bd139e44bdd3e619ed02ebf3d6f62a6
Jagdishcoder <Jagdishfblb@gmail.com>
2024-11-01 06:26:33 +0530

    testimonial remove

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "./components/herosection";
 import KeyBenefits from "./components/keybenefits";
 import HowItWorks from "./components/howitworks";
 import Servicesoverview from "@/app/components/servicesovr"
-import Testimonials from "./components/testimonial";
+// import Testimonials from "./components/testimonial";
 import ResourcesBlog from "./components/blog";
 import CallToAction from "./components/calltoaction";
 
@@ -15,7 +15,7 @@ export default function Home() {
     <KeyBenefits />
     <HowItWorks />
     <Servicesoverview />
-    <Testimonials /> 
+    {/* <Testimonials />  */}
     <ResourcesBlog />
     <CallToAction />
      </div>
